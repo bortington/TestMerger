3d00cf03d0ca55f37cf1b5ecbe82a8a024a03d26
Alek Ivankovic <alek.ivankovic@sencom.com.au>
2016-04-16 15:59:30 +0800

    Not much

---
+++ UnityProject/Assets/Scripts/Gripper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Keeps the character anchored to vertical walls and ceilings
+/// </summary>
+public class Gripper : MonoBehaviour
+{
+    public Vector2 RaycastDirection = Vector2.down;
+
+    /// <summary>
+    /// Called when draw gizmos selected occurs.
+    /// </summary>
+    private void OnDrawGizmosSelected()
+    {
+
+    }
+}
